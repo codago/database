c50aba2c9830fc01d98c943a599e0adc76f7273d
Firmansyah <firmansyahas@hotmail.com>
2017-09-11 14:40:07 +0700

    nomer 14

--- database.sql
+++ database.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE university.db
 
 	CREATE TABLE `mahasiswa` ( --membuat table untuk Mahasiswa
-		`nim`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		`idmahasiswa`	INTEGER NOT NULL,
+		`nim`	INTEGER NOT NULL,
 		`nama`	TEXT NOT NULL,
 		`alamat`	TEXT NOT NULL,
 		`jurusan`	TEXT NOT NULL
+		PRIMARY KEY(idmahasiswa,nim)
 	);
 
 	CREATE TABLE `jurusan` ( --membuat table untuk Jurusan
@@ -23,40 +25,42 @@ CREATE DATABASE university.db
 		`sks`	INTEGER NOT NULL
 	);
 
-	CREATE TABLE `relationship` ( --membuat table untuk Relationship
-		`idrelationship`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		`iddosen`	INTEGER NOT NULL,
-		`nim`	INTEGER NOT NULL,
-		`idmatkul`	INTEGER NOT NULL,
-		`nilai`	INTEGER NOT NULL
-	);
+	CREATE TABLE `relationship` (
+	`idrelationship`	INTEGER NOT NULL,
+	`iddosen`	INTEGER NOT NULL,
+	`nim`	INTEGER NOT NULL,
+	`idmatkul`	INTEGER NOT NULL,
+	`nilai`	INTEGER NOT NULL,
+	PRIMARY KEY(idrelationship,iddosen,nim,idmatkul)
+);
 
 
 --Tambah Mahasiswa
-INSERT INTO `mahasiswa`(`nim`,`nama`,`alamat`,`jurusan`) VALUES (012201300001,'Firmansyah','Jakarta','Sistem Informasi');
-INSERT INTO `mahasiswa`(`nim`,`nama`,`alamat`,`jurusan`) VALUES (012201300002,'Mikha','Bekasi','Hukum');
-INSERT INTO `mahasiswa`(`nim`,`nama`,`alamat`,`jurusan`) VALUES (012201300003,'Toni','Surabaya','DKV');
+INSERT INTO `mahasiswa`(`nim`, `nama`,`alamat`,`jurusan`) VALUES ('01','Firmansyah','Jakarta','Sistem Informasi');
+INSERT INTO `mahasiswa`(`nim`, `nama`,`alamat`,`jurusan`) VALUES ('02','Mikha','Bekasi','Hukum');
+INSERT INTO `mahasiswa`(`nim`, `nama`,`alamat`,`jurusan`) VALUES ('03','Toni','Jakarta','Komputer');
+
 
 --Tambah Jurusan
-INSERT INTO `jurusan`(`idjurusan`,`nama`) VALUES ('001','Sastra Indonesia');
-INSERT INTO `jurusan`(`idjurusan`,`nama`) VALUES ('002','Sastra Jerman');
-INSERT INTO `jurusan`(`idjurusan`,`nama`) VALUES ('003','Sastra Arab');
+INSERT INTO `jurusan`(`nama`) VALUES ('Sastra Indonesia');
+INSERT INTO `jurusan`(`nama`) VALUES ('Sastra Jerman');
+INSERT INTO `jurusan`(`nama`) VALUES ('Sastra Arab');
 
 --Tambah Dosen
-INSERT INTO `dosen`(`iddosen`,`nama`) VALUES ('0001','Nur');
-INSERT INTO `dosen`(`iddosen`,`nama`) VALUES ('0002','Rila Mandala');
-INSERT INTO `dosen`(`iddosen`,`nama`) VALUES ('0003','Antonius');
+INSERT INTO `dosen`(`nama`) VALUES ('Nur');
+INSERT INTO `dosen`(`nama`) VALUES ('Rila Mandala');
+INSERT INTO `dosen`(`nama`) VALUES ('Antonius');
 
 
 --Tambah Mata_Kuliah
-INSERT INTO `matakuliah`(`idmatkul`,`nama`,`sks`) VALUES (01,'Agama','4');
-INSERT INTO `matakuliah`(`idmatkul`,`nama`,`sks`) VALUES (03,'Bahasa Indonesia','3');
-INSERT INTO `matakuliah`(`idmatkul`,`nama`,`sks`) VALUES (02,'Komputer','4');
+INSERT INTO `matakuliah`(`nama`,`sks`) VALUES (Agama,'4');
+INSERT INTO `matakuliah`(`nama`,`sks`) VALUES ('Bahasa Indonesia','3');
+INSERT INTO `matakuliah`(`nama`,`sks`) VALUES ('Komputer','4');
 
 --Tambah Relationship
-INSERT INTO `Relationship`(`idrelationship`,`iddosen`,`nim`, `idmatkul`,`nilai`) VALUES (2, 0002, 012201300002, 002, 100);
-INSERT INTO `Relationship`(`idrelationship`,`iddosen`,`nim`, `idmatkul`,`nilai`) VALUES (1, 0001, 012201300001, 001, 80);
-INSERT INTO `Relationship`(`idrelationship`,`iddosen`,`nim`, `idmatkul`,`nilai`) VALUES (3, 0003, 012201300003, 003, 100);
+INSERT INTO `Relationship`(`iddosen`,`nim`, `idmatkul`,`nilai`) VALUES ('0002', '01', '002', 100);
+INSERT INTO `Relationship`(`iddosen`,`nim`, `idmatkul`,`nilai`) VALUES ('0001', '02', '001', 80);
+INSERT INTO `Relationship`(`iddosen`,`nim`, `idmatkul`,`nilai`) VALUES ('0003', '03', '003', 100);
 
 --untuk nilai
 select mahasiswa.nama, matakuliah.nama as nama_mata_kuliah, dosen.nama as dosen, nilai from relationship,
